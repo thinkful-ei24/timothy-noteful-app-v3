48bd313d45ccc186d59fd08d82959888fa52a9e0
Timothy <superfluous11@gmail.com>
2018-09-18 12:22:18 -0700

    Updated queries.js

--- models/note.js
+++ models/note.js
@@ -10,4 +10,12 @@ const notesSchema = new mongoose.Schema(
 // add createdAt and updatedAt fields
 notesSchema.set('timestamps', true);
 
+noteSchema.set('toObject', {
+    virtuals: true,     // include built-in virtual `id`
+    versionKey: false,  // remove `__v` version key
+    transform: (doc, ret) => {
+      delete ret._id; // delete `_id`
+    }
+  });
+
 module.exports = mongoose.model('Note', notesSchema);
---
+++ scratch/queries.js
@@ -0,0 +1,97 @@
+const mongoose = require('mongoose');
+const { MONGODB_URI } = require('../config');
+
+const Note = require('../models/note');
+
+// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//   .then(() => {
+//     const searchTerm = 'lady gaga';
+//     let filter = {};
+
+//     if (searchTerm) {
+//       filter.title = { $regex: searchTerm };
+//     }
+
+//     return Note.find(filter).sort({ updatedAt: 'desc' });
+//   })
+//   .then(results => {
+//     console.log(results);
+//   })
+//   .then(() => {
+//     return mongoose.disconnect()
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
+
+// get notes by id query
+//   mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//     .then(() => {
+//         return Note.findOne()
+//     })
+//     .then(note => {
+//         const id = note._id;
+//         return Note.findById(id)
+//     })
+//     .then(note => {
+//         console.log(note);
+//     })
+//     .then(() => mongoose.disconnect())
+//     .catch(err => console.log(err));
+
+// create new notes
+// const newNote = {
+//     title: "Foo",
+//     content: "Bar"
+// };
+// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//     .then(() => {
+
+//         return Note.create(newNote)
+//     })
+//     .then(note => {
+//         console.log(note);
+//     })
+//     .then(() => mongoose.disconnect())
+//     .catch(err => console.error(err));
+
+// update a new note using Note.findByIdAndUpdate
+// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//     .then(() => {
+//         return Note.findOne();
+//     })
+//     .then(note => {
+//         const id = note._id;
+//         const update = { 
+//             title: "New title", 
+//             content: "New content"
+//         };
+//         return Note.findByIdAndUpdate(id, {$set: update});
+//     })
+//     .then(note => {
+//         console.log(note);
+//     })
+//     .then(() => {
+//         mongoose.disconnect();
+//     })
+//     .catch(err => console.error(err));
+
+// delete a note by id 
+
+let id; 
+mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+    .then(() => {
+         return Note.findOne();     
+    })
+    .then(note => {
+        id = note._id;
+        console.log(id);
+        return Note.findByIdAndRemove(id);
+    })
+    .then(() => {
+        return Note.findById(id).count();
+    })
+    .then(count => console.log(count))
+    .catch(err => console.error(err));
