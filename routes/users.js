e36da06d7a490d66370d139f5435da27108fd170
Timothy <superfluous11@gmail.com>
2018-09-24 12:17:20 -0700

    Updated post user endpoint

--- routes/users.js
+++ routes/users.js
@@ -3,36 +3,65 @@ const User = require('../models/user');
 
 const router = express.Router();
 
+function validationError(message){
+  const err = new Error(message);
+  err.status = 422;
+  return err;
+}
+
 router.post('/', (req, res, next) => {
   const { username, password, fullName } = req.body;
 
   const requireFields = ['username', 'password'];
   const missingField = requireFields.find(field => (!(field in req.body)));
   
-  if(missingField) return res.status(422).res.json({
-    reason: 'ValidationError',
-    message: 'Missing field',
-    location: missingField
-  });
+  if(missingField) {
+    const err = validationError(`${missingField} field is missing`);
+    return next(err);
+  }
 
   const trimmedFields = requireFields;
-  const nontrimmedField = trimmedFields.find(field => req.body[field] !== req.body[field]);
-  if(nontrimmedField) return res.status(422).res.json({
-    reason: 'ValidationError',
-    message: 'Cannot start or end with whitespace',
-    location: nontrimmedField
-  });
+  const nontrimmedField = trimmedFields.find(field => req.body[field].trim() !== req.body[field]);
+  if(nontrimmedField) {
+    const err = validationError(`${nontrimmedField} cannot start or end with whitespace`);
+    return next(err);
+  }
+
+  const sizedFields = {
+    username: {
+      min: 1
+    },
+    password: {
+      min: 8,
+      max: 72
+    }
+  };
+
+  const tooSmallField = Object.keys(sizedFields).find(field => 
+    'min' in sizedFields[field] && req.body[field].trim().length < sizedFields[field].min
+  );
+
+  const tooLargeField = Object.keys(sizedFields).find(field => 
+    'max' in sizedFields[field] && req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if(tooSmallField || tooLargeField) {
+    const message = ( 
+      tooSmallField
+        ? `${tooSmallField} must be at least ${sizedFields[tooSmallField].min} characters` 
+        : `${tooLargeField} must be at most ${sizedFields[tooLargeField].max} characters`
+    );
+    const err = validationError(message);
+    return next(err);
+  }
 
   User.find({ username })
     .countDocuments()
     .then(count => {
-      if(count > 0) return Promise.reject(
-        {
-          reason: 'ValidationError',
-          message: 'Username exists',
-          location: 'username'
-        }
-      );
+      if(count > 0) {
+        const err = validationError('Username already exists');
+        return Promise.reject(err);
+      }
 
       return User.hashPassword(password);
     })
@@ -44,13 +73,9 @@ router.post('/', (req, res, next) => {
       });
     })
     .then(user => {
-      return res.status(201)
-        .json(user);
+      return res.status(201).json(user);
     })
-    .catch(err => {
-      if(err.reason === 'ValidationError') return res.status(422).json(err);
-      next(err);
-    });
+    .catch(next);
 });
 
 
